125f99c351e0ee7d89ac60955405e5499286d7a7
Frank Bardon Jr <frank@nerdsrescue.me>
2012-11-06 15:58:33 -0500

    Updated page model

--- library/application/Application/Model/Page.php
+++ library/application/Application/Model/Page.php
@@ -1,7 +1,23 @@
 <?php
 
+/**
+ * Nerd-CMS Model namespace
+ *
+ * @package Nerd-CMS
+ * @subpackage Models
+ */
 namespace Application\Model;
 
+/**
+ * Nerd-CMS Page Model
+ *
+ * Handles data pertaining to Nerd-CMS pages. The data access layer for pages
+ * is fairly complex, and we do our best to keep this complexity to a minimum,
+ * but it's difficult.
+ *
+ * @package Nerd-CMS
+ * @subpackage Models
+ */
 class Page extends \Nerd\Model
 {
     const DELIMITER = '@@';
@@ -10,17 +26,24 @@ class Page extends \Nerd\Model
 
     protected static $table = 'nerd_pages';
 
+    /**
+     * Search pages
+     *
+     * @param    string          Search term
+     * @param    string          Sort results by...
+     * @return   this
+     */
     public static function search($search, $order = '`title` ASC')
     {
         $search  = strtolower("{$search}%");
         $sql     = <<<SQL
-SELECT *
-  FROM `nerd_pages`
-  WHERE
-       LOWER(`title`)    LIKE ?
-    OR LOWER(`subtitle`) LIKE ?
-    OR LOWER(`uri`)      LIKE ?
-  ORDER BY $order
+            SELECT *
+              FROM `nerd_pages`
+              WHERE
+                   LOWER(`title`)    LIKE ?
+                OR LOWER(`subtitle`) LIKE ?
+                OR LOWER(`uri`)      LIKE ?
+              ORDER BY $order
 SQL;
 
         return static::findAll($sql, $search, $search, $search);
@@ -43,7 +66,7 @@ SQL;
      */
     public function delete()
     {
-        if (strpos($this->uri, '@@') !== false) {
+        if ($this->isSpecialUri()) {
             throw new \Nerd\DB\Exception('You are not permitted to delete special URIs.');
         }
 
@@ -53,6 +76,10 @@ SQL;
     /**
      * Form builder overload
      *
+     * Automatic form creation does not completely compensate for the what is
+     * required to display the pages form. We need to transform some of the
+     * form elements, set default values, among many other things.
+     * 
      * @todo Incorporate site lookup into site id call
      *
      * @see Nerd\Model::form()
@@ -69,36 +96,28 @@ SQL;
         $site = $form->findByAttribute('id', 'page_site_id')
             ->type('hidden')
             ->wrap(false)
-            ->wrapField(false)
-            ->value(1);
+            ->wrapField(false);
 
             unset($site->label);
 
         // We need to transform the layout_id field to a select
         $layout = $form->findByAttribute('id', 'page_layout_id')->remove();
 
-        // Steal options from current layout form object.
-        $options = $layout->option('options');
-        unset($options['type'], $options['maxlength'], $layout);
-        $options['options'] = [
-            'one' => 'One',
-            'two' => 'Two',
-        ];
+        // Steal attributes from current layout form object.
+        $options = $layout->attributes();
+        unset($options['type'], $options['maxlength'], $options['value'], $layout);
+        $options['options'] = \Theme\Theme::instance()->layouts;
 
         // Create a new select field with layout options.
         $form->field('select', $options, true)
             ->selected($this->layout_id ?: null)
-            ->label('Layout', ['class' => 'control-label']);
+            ->label('Layout');
 
         return $form;
     }
 
-    public function isSpecialUri($uri = null)
+    public function isSpecialUri()
     {
-        if ($uri === null) {
-            $uri = $this->uri;
-        }
-
-        return strpos($uri, '@@') !== false;
+        return strpos($this->uri, static::DELIMITER) !== false;
     }
 }
