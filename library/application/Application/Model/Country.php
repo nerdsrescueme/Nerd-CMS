ca0b1d0fb2500bb0e2a39eb3bd110e634c698262
Frank Bardon Jr <frank@nerdsrescue.me>
2012-11-28 14:09:10 -0500

    Added country model

---
+++ library/application/Application/Model/Country.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Application\Model;
+
+class Country extends \Nerd\Model
+{
+    protected static $table = 'nerd_countries';
+    protected static $columns;
+    protected static $constraints;
+    protected static $columnNames;
+    protected static $primary;
+}
